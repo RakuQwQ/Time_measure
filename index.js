a4101d9c40fdc00819c3b69441deb7b6122b3e4a
RakuQwQ <107921688+RakuQwQ@users.noreply.github.com>
2024-08-18 02:47:55 +0800

    Update index.js

--- index.js
+++ index.js
@@ -53,8 +53,9 @@ document.getElementById("remove_time").onclick = function clock(){
     let time_amount = document.getElementById("time_modify").value;
     let time_unit = document.getElementById("time_unit").value;
 
-    if (total_time > time_amount){
     time_amount = time_amount * time_unit; 
+    
+    if (total_time > time_amount){
     total_time -= time_amount;
     } else if (total_time <= time_amount){
         total_time = 0;
